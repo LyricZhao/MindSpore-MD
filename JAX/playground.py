12e2beb5a37aa9674befb3292e21f785abe51285
LyricZhao <lyricz@yeah.net>
2020-07-22 22:00:47 +0800

    Add some comments for displacement, shift and energy_fn

--- JAX/playground.py
+++ JAX/playground.py
@@ -2,12 +2,15 @@ from jax import random
 from jax_md import space, energy, simulate
 
 # configs
-N = 32
+N = 16
 dt = 1e-1
 temperature = 0.1
 box_size = 5.0
 
-# displacement:
+# R: current position
+# dR: displacement
+# displacement(Ra, Rb):
+#   dR = Ra - Rb
 #   np.mod(dR + side * f32(0.5), side) - f32(0.5) * side
 # periodic shift:
 #   np.mod(R + dR, side)
@@ -15,14 +18,19 @@ box_size = 5.0
 #   R + dR
 displacement, shift = space.periodic(box_size)
 
-# TODO: figure it out
+# dr: pairwise distances
+# epsilon: Interaction energy scale (const)
+# alpha: interaction stiffness
+# dr = distance(R)
+# U(dr) = np.where(dr < 1.0, (1 - dr) ** 2, 0)
+# energy_fn(R) = diagonal_mask(U(dr))
 energy_fn = energy.soft_sphere_pair(displacement)
 
 # simulate
 pos_key, sim_key = random.split(random.PRNGKey(0))
-pos = random.uniform(pos_key, (N, 2), maxval=box_size)
+R = random.uniform(pos_key, (N, 2), maxval=box_size)
 init_fn, apply_fn = simulate.brownian(energy_fn, shift, dt, temperature)
-state = init_fn(sim_key, pos)
+state = init_fn(sim_key, R)
 for i in range(1000):
     if not i or (i + 1) % 100 == 0:
         print('Running iteration {} ...'.format(i + 1))
